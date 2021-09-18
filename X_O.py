85410d1bccf2cbf51a1a4c08782650f866ce6b46
KovalStas1 <kovalstas88@gmail.com>
2021-09-18 15:30:06 +0300

    First test commit

--- X_O.py
+++ X_O.py
@@ -1,7 +1,7 @@
 table = [[1,2,3],[4,5,6],[7,8,9]]
 for i in table:
     print(i)
-
+#inital teable
 table[0][0] == 1, table[0][1] == 2, table[0][2] == 3
 table[1][0] == 4, table[1][1] == 5, table[1][2] == 6
 table[2][0] == 7, table[2][1] == 8, table[2][2] == 9
